e31500b7c6b5271e7f362636a7b9fa4ec060ecb3
Mobilize2017 <30937129+Mobilize2017@users.noreply.github.com>
2017-08-13 18:54:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,19 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
